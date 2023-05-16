3c227efb618d2b8059b7fe34cdd9a4af19e5a422
萌面人 <xiejiashu@outlook.com>
2023-05-16 09:26:16 +0800

    第一个MOD建仓

---
+++ GodBrain/Project.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Verse;
+
+namespace GodBrain
+{
+    public class HeadiffComp_GodBrain : HediffComp
+    {
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            // 获取人物的身体部位列表
+            var parts = this.Pawn.health.hediffSet.GetNotMissingParts();
+
+            // 遍历每个身体部位
+            foreach (var part in parts)
+            {
+                // 跳过大脑部位
+                if (part.def.defName == "Brain") continue;
+
+                // 获取身体部位的 hediffs
+                var hediffs = this.Pawn.health.hediffSet.hediffs;
+
+                // 在 hediffs 中查找对应的身体部位
+                var hediff = hediffs.FirstOrDefault(h => h.Part == part);
+
+                // 如果找到了 hediff，则修改它的效率
+                if (hediff != null)
+                {
+                    // 将身体部位的效率设置为 150%
+                    hediff.def.addedPartProps.partEfficiency = 99999.0f;
+                }
+            }
+        }
+    }
+}
